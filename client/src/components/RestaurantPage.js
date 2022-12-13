9f823d3faff32bb1b7e15516a9ec6003b4e41ecb
Agould94 <77901585+Agould94@users.noreply.github.com>
2022-12-13 17:56:09 -0500

    added restaurant page details

--- client/src/components/RestaurantPage.js
+++ client/src/components/RestaurantPage.js
@@ -1,10 +1,33 @@
 import React, {useState} from "react";
 
-function RestaurantPage(){
+function RestaurantPage({restaurant}){
+
+    
+    let reviewsToDisplay 
+    if(restaurant.reviews){
+        reviewsToDisplay = restaurant.reviews.map((review)=>{
+        return(
+        <div>
+            <p>{review.stars}</p>
+            <p>{review.content}</p>
+        </div>
+        )}
+    )}
+
 
     return(
         <div>
-            hello
+            <h1>{restaurant.name}</h1>
+            <br/>
+            <h2>{restaurant.vibe}</h2>
+            <p>{restaurant.food_type}</p>
+            <p>{restaurant.address}</p>
+            <p>{restaurant.phone_number}</p>
+            {restaurant.reviews ? 
+                reviewsToDisplay
+                :
+                null
+            }
         </div>
     )
 }
